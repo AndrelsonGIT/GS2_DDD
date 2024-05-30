aa5537a155153db65dd28dcd76edc2f1b316fb65
WedSan <andre.alves.silvazesez@gmail.com>
2024-05-30 20:02:59 -0300

    initial commit

---
+++ src/test/java/fiap/codecraft/CodeCraftApplicationTests.java
@@ -0,0 +1,13 @@
+package fiap.codecraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeCraftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
